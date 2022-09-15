39a4a915e0ca6bc6fc72d3168b9067cd1a563f2d
Guilherme <mttsguil@gmail.com>
2022-09-15 12:08:56 -0300

    add: css model

---
+++ src/pages/stylise.jsx
@@ -0,0 +1,9 @@
+import styles from './Stylise.module.css'
+
+export default function Stylise() { 
+    return( 
+        <div className={styles.purple}>
+            <h1>Estilo usando modulos</h1>
+        </div>
+    )
+}
